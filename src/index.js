50578355d12a0ba9e3d2f8cc33c967a8d1fb7e75
jerome creignou <jerome.creignou@gmail.com>
2014-03-17 23:54:46 +0100

    remove usused lines

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@
 var log4js = require('log4js');
 
 module.exports = function (options, imports, register) {
-
     var logger = options.config ? log4js.configure(options.config) : log4js;
     register(null, {log: logger});
 };
