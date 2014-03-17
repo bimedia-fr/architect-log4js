e7eef0bfb664d2fdc4e17b126c685d3ca2f4e7a0
jerome creignou <jerome.creignou@gmail.com>
2014-03-18 00:48:21 +0100

    fix undefined with service configuration

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@
 var log4js = require('log4js');
 
 module.exports = function (options, imports, register) {
-    var logger = options.config ? log4js.configure(options.config) : log4js;
-    register(null, {log: logger});
+    if (options.config) {
+        log4js.configure(options.config);
+    }
+    register(null, {log: log4js});
 };
