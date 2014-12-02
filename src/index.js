d4bca40fe5f3d6b62d7b87d1b11c3b1e5d0b0ce3
jerome creignou <jerome.creignou@gmail.com>
2014-12-02 14:42:52 +0100

    plug logger on hub log.error and log.info events

--- src/index.js
+++ src/index.js
@@ -4,8 +4,19 @@
 var log4js = require('log4js');
 
 module.exports = function (options, imports, register) {
+    var hub = imports.hub;
+
     if (options.config) {
         log4js.configure(options.config);
     }
+
+    var logger = log4js.getLogger('app');
+
+    hub.on('log.error', logger.error.bind(logger));
+    hub.on('log.info', logger.info.bind(logger));
+
     register(null, {log: log4js});
 };
+
+module.exports.provides = ['log'];
+module.exports.consumes = ['hub'];
