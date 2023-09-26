636816c418dcfc5a10f5ae65190da86d510f9a8a
Sébastien Hus <s.hus@bimedia.com>
2023-09-26 17:57:44 +0200

    Simplification algo

--- src/request-logger.js
+++ src/request-logger.js
@@ -60,11 +60,8 @@ module.exports = function (config, log4js) {
                 var res = wrapLoggers({}, function (level) {
                     return function () {
                         var args = [].slice.apply(arguments);
-                        if(args[0] instanceof Error) {
-                            args[0] = util.format(format, ppty, args[0]);
-                        }
-                        else if(!(args[0] instanceof Error) && typeof args[0] === 'object') {
-                            args[0] = util.format(format, ppty, JSON.stringify(args[0]));
+                        if(typeof args[0] === 'object') {
+                            args[0] = util.format(format, ppty, args[0] instanceof Error ? args[0] : JSON.stringify(args[0]));
                         }
                         else {
                             args[0] = util.format(format, ppty, String(args[0]));
